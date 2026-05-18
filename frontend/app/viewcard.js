ce842fad02a9eb9774404fb7e0107480a45dbef4
sarayu19724 <madigundusarayu@gmail.com>
2026-05-19 03:53:49 +0530

    Update viewcard.js

--- frontend/app/viewcard.js
+++ frontend/app/viewcard.js
@@ -48,7 +48,7 @@ const Viewcard = ({ open, course, onUpdate, onDelete, onClose }) => {
       const token = localStorage.getItem("jwtToken");
       console.log("Updating course with values:", values); // Log values being sent
       const response = await axios.put(
-        `http://localhost:5000/courses/${course.id}`,
+        `https://course-helper-woad.vercel.app/courses/${course.id}`,
         values,
         {
           headers: {
