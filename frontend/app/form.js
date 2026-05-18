11f47f7ae1be13cffd0d772bacdfb4c25d1dbe43
sarayu19724 <madigundusarayu@gmail.com>
2026-05-19 04:07:02 +0530

    Use environment variable for API URL in form.js

--- frontend/app/form.js
+++ frontend/app/form.js
@@ -6,7 +6,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import axios from 'axios';
-
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const DialogForm = ({ open, onClose, onSuccess }) => {
   const [values, setValues] = useState({
     coursename: '',
@@ -31,7 +31,7 @@ const DialogForm = ({ open, onClose, onSuccess }) => {
     try {
       const token = localStorage.getItem("jwtToken");
       const response = await axios.post(
-        "https://course-helper-woad.vercel.app/courses",
+        "${API_URL}/courses",
         values,
         {
           headers: {
