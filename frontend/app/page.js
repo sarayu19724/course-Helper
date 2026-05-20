ba5330430742d515d0a345435d97450478e1ee0c
sarayu19724 <madigundusarayu@gmail.com>
2026-05-20 07:00:56 +0530

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
 
     const fetchCourses = async () => {
       try {
-        const response = await axios.get('${process.env.NEXT_PUBLIC_API_URL}/courses');
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/courses`);
         setCourses(response.data || []);
       } catch (error) {
         console.error("Error fetching courses:", error);
@@ -70,7 +70,7 @@ export default function Home() {
   };
 
   const handleAddCourseSuccess = async () => {
-    const response = await axios.get('http://localhost:5000/courses');
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/courses`);
     setCourses(response.data || []);
   };
 
